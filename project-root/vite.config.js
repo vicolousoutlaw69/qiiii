333f849ca3515b8997d1c4a8daa32f1acb58d7eb
vicolousoutlaw69 <rakesh200674kumar@gmail.com>
2025-04-08 11:50:19 +0530

    Delete project-root directory

--- project-root/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: './', // this is important for relative file serving
-  plugins: [react()],
-});
